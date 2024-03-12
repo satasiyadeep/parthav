ba911216589b129a658e0f79a6a60c74c21c6ca9
satasiyadeep <144014756+satasiyadeep@users.noreply.github.com>
2024-03-12 22:45:27 +0530

    xr

---
+++ src/app.js
@@ -0,0 +1,30 @@
+const express=require("express")
+const app=express()
+const path=require("path")
+const hbs=require("hbs")
+const collection=require("./src/mongodb")
+
+app.use(express.static("public"))
+app.use(express.json())
+
+app.set("view engine","hbs")
+const templatePath=path.join(__dirname,'views');
+app.set("views",templatePath)
+
+app.use(express.urlencoded({extended:false}))
+app.use(express.static('public'));
+app.get("/",(req,res)=>{
+  res.render("index");
+})
+
+app.get("/Register.hbs",(req,res)=>{
+  res.render("Register")
+})
+
+
+
+app.listen(4000,()=>{
+  console.log("port connected");
+})  
+ 
+    
