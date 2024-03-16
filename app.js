95cfe23f13a2eaf1d8ccb4f28c17a0661d3756ce
satasiyadeep <144014756+satasiyadeep@users.noreply.github.com>
2024-03-16 12:14:15 +0530

    yes

--- app.js
+++ app.js
@@ -4,12 +4,16 @@ const path=require("path")
 const hbs=require("hbs")
 const collection=require("./src/mongodb")
 
+
+
 app.use(express.static("public"))
 app.use(express.json())
 
 app.set("view engine","hbs")
 const templatePath=path.join(__dirname,'views');
+const partialPath=path.join(__dirname,'views/partials');
 app.set("views",templatePath)
+hbs.registerPartials(partialPath);
 
 app.use(express.urlencoded({extended:false}))
 app.use(express.static('public'));
@@ -17,13 +21,65 @@ app.get("/",(req,res)=>{
   res.render("index");
 })
 
-app.get("/login.hbs",(req,res)=>{
+app.get("/login",(req,res)=>{
   res.render("login")
 })
-app.get("/index.hbs",(req,res)=>{
+app.get("/book",(req,res)=>{
+  
+  res.render("book")
+  
+})
+app.get("/index",(req,res)=>{
   res.render("index")
 })
+app.get("/about",(req,res)=>{
+  res.render("about")
+})
+app.get("/Register",(req,res)=>{
+  res.render("Register")
+})
 
+
+app.post("/Register",async (req,res)=>{
+  const data={
+
+    fullname:req.body.name,
+    email:req.body.email,
+    username:req.body.username,
+    password:req.body.pass,
+    
+  }
+
+  try {
+    // Compare passwords using strict equality (===)
+    if (req.body.pass === req.body.repeatpass) {
+      await collection.insertMany(data); // Use insertOne instead of insertMany for a single document
+      res.render("book");
+      // Assuming collection is properly defined
+    } else {
+      res.send("Passwords do not match");      
+    }
+  } catch (error) {
+    console.error(error);
+    res.send("Error occurred while processing your request");
+  }
+});
+app.post("/login",async (req,res)=>{
+    
+  try{
+    const check=await collection.findOne({email:req.body.email,password:req.body.pass});
+    if(check){
+      res.render("book")
+    }
+    else{
+      res.send("wrong password")
+    }
+ }catch{
+  res.send(" wrong detail ")
+ }
+
+
+})
 
 app.listen(4000,()=>{
   console.log("port connected");
--- src/mongodb.js
+++ src/mongodb.js
@@ -8,3 +8,29 @@ mongoose.connect("mongodb://localhost:27017/loginsignuptutorial")
   console.log("fail to connect")
 })
 
+const LoginSchema=new mongoose.Schema({
+  fullname: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+ 
+  
+})
+
+const collection=new mongoose.model("collection1",LoginSchema)
+
+module.exports=collection
